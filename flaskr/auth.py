5d570ff16137637a005845fffbea819bdbdb2d19
Santi <santyespi2006@gmail.com>
2023-08-23 09:09:05 -0300

    hice el registro y el login

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -70,13 +70,7 @@ def load_logged_in_user():
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-
-@bp.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('index'))
+        g.user = get_db().execute('SELECT * FROM user WHERE id = ?', (user_id,)).fetchone()
 
 @bp.route('/logout')
 def logout():
